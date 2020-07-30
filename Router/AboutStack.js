220d9628302c5ebe9005fd0dc9a4857574b93bab
Emnetdegafe <emnetdegafe@gmail.com>
2020-07-30 22:46:32 +0200

    Wrapping about page in stack navigator

---
+++ Router/AboutStack.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import About from "../screens/About";
+import Header from "../shared/Header";
+
+const Stack = createStackNavigator();
+
+export default function AboutStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="About"
+        component={About}
+        options={{
+          title: "About Alles Over Film",
+          headerStyle: {
+            backgroundColor: "#e40066",
+          },
+          headerTitleStyle: {
+            textAlign: "center",
+            color: "#ffffff"
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
